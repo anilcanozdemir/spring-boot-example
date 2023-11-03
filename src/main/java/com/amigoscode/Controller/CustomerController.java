f73a8f1cb4cd0fe3e571f6b5bc6c40a928abc41f
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-11-04 00:00:08 +0300

    Update CustomerController.java

--- src/main/java/com/amigoscode/Controller/CustomerController.java
+++ src/main/java/com/amigoscode/Controller/CustomerController.java
@@ -1,4 +1,24 @@
 package com.amigoscode.Controller;
 
+
+import com.amigoscode.Entity.Customer;
+import com.amigoscode.Repository.CustomerRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @PostMapping("/add")
+    void addCustomer(Customer customer)
+    {
+        customerRepository.save(customer);
+    }
 }
